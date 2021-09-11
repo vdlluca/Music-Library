bb4a75ed66030c7f448801b63e36254cf16ba79a
lux <luca.iacovella@protonmail.com>
2021-09-11 17:59:52 +0200

    healthcheck added to seedbox

--- seedbox/start.sh
+++ seedbox/start.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
 find torrents -name "*.added" | sed -E "s,(.*).added,mv \"&\" \"\1\"," | sh
-rm -rf /transmission/conf.d/resume /transmission/conf.d/torrents
-
+rm -rf /transmission/conf.d/torrents
 transmission-daemon -g /transmission/conf.d -f
